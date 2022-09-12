3edd17ca91a798b2a7dd3db6b4b2015cacd0d31a
Shyam3097 <97452238+Shyam3097@users.noreply.github.com>
2022-09-12 15:01:12 +0530

    Modified authorController

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -5,33 +5,48 @@ const createAuthor = async function (req, res) {
     let data = req.body;
     let { fname, lname, email, password } = req.body;
     const reg = /^[a-zA-Z]*$/;
-    const emailReg = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
-    
+    const emailReg =
+      /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+    const passReg =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
     //fname validation
-    
+
     if (!fname) {
-      return res.status(400).send({ status: false, msg: "please enter firstName " });
+      return res
+        .status(400)
+        .send({ status: false, msg: "please enter firstName " });
     }
 
     if (!fname.match(reg)) {
-      return res.status(400).send({ status: false, msg: "Accept only Alphabets" });
+      return res
+        .status(400)
+        .send({ status: false, msg: "Accept only Alphabets" });
     }
 
     if (fname.length <= 2) {
-      return res.status(400).send({ msg: "fname length is too short", status: false });
+      return res
+        .status(400)
+        .send({ status: false, msg: "fname length is too short" });
     }
 
     //lname validation
-    
+
     if (!lname) {
-      return res.status(400).send({ status: false, msg: "please enter lastName " });
+      return res
+        .status(400)
+        .send({ status: false, msg: "please enter lastName " });
     }
 
     if (!lname.match(reg)) {
-      return res.status(400).send({ msg: "Accept only Alphabets", status: false });
+      return res
+        .status(400)
+        .send({ status: false, msg: "Accept only Alphabets" });
     }
     if (lname.length <= 2) {
-      return res.status(400).send({ msg: "lname length is too short", status: false });
+      return res
+        .status(400)
+        .send({ status: false, msg: "lname length is too short" });
     }
 
     //email validation
@@ -41,26 +56,58 @@ const createAuthor = async function (req, res) {
     }
 
     if (!email.match(emailReg)) {
-      return res.status(400).send({ msg: "eg. Name@gmail.com", status: false });
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "Please provide the valid email like --> Name@gmail.com",
+        });
+    }
+
+    //Title validation
+
+    if (!title) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Title isn't being provided" });
+    }
+    if (!title == "Mr" || !title == "Mrs" || !title == "Miss") {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "please select from the enum value like ( Mr or Mrs or Miss )",
+        });
     }
 
     //password validation
-    
+
     if (!password) {
-      return res.status(400).send({ status: false, msg: "password is missing" });
+      return res
+        .status(400)
+        .send({ status: false, msg: "password is missing" });
     }
 
     if (password.length < 8) {
       return res
         .status(400)
-        .send({ msg: "enter atleast 8 characters in password", status: false });
+        .send({ status: false, msg: "enter atleast 8 characters in password" });
+    }
+
+    if (!password.match(passReg)) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "Sorry, password shall carry at least one uppercase letter, one lowercase letter, one number and one special character",
+        });
     }
 
     //happy code
 
     let savedData = await authorModel.create(data);
 
-    return res.status(201).send({ data: savedData });
+    return res.status(201).send({ status: true, data: savedData });
   } catch (err) {
     return res.status(500).send({ msg: "Error", error: "invalid request" });
   }
