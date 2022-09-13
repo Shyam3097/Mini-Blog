4afd88fd9716418c316918eacccebc095e6b5e06
Shyam3097 <97452238+Shyam3097@users.noreply.github.com>
2022-09-14 02:02:40 +0530

    modified push

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -111,37 +111,36 @@ const getBlogs = async (req, res) => {
 
 const update = async function (req, res) {
   try {
-    const blogIdData = req.params.blogId;
-    const data = req.body;
+    let blogidData = req.params.blogId;
+    let data = req.body;
     const { title, body, tags, category, subcategory } = data;
 
     const findData = await blogModel.findByIdAndUpdate(
-      { _id: blogIdData },
+      { _id: blogidData },
       {
         $addToSet: { tags: tags, subcategory: subcategory },
         $set: {
           title: title,
           body: body,
           category: category,
-          publishedAt: Date.now(),
           isPublished: true,
+          publishedAt: Date.now(),
         },
-      }
+      },
+      { new: true }
     );
 
-    if (!findData) {
-      return res.status(404).send({ status: false, msg: "Blog Not Found" });
+    // console.log(findData)
+    if (!findData || findData.isDeleted == true) {
+      res.status(404).send({ msg: "Not found", status: false });
     }
 
-    if (findData.isDeleted == true) {
-      return res
-        .status(404)
-        .send({ status: false, msg: "Blog already deleted" });
-    }
-
-    return res.status(200).send({ status: true, data: updatedBlog });
-  } catch (err) {
-    res.status(400).send({ status: "Failed to update", msg: err.message });
+    res.status(200).send({ msg: findData, status: true });
+  } catch (error) {
+    res.status(400).send({
+      status: "Failed to update",
+      message: error.message,
+    });
   }
 };
 
@@ -182,13 +181,14 @@ const deleteBlogByQuery = async function (req, res) {
     }
     let blogData = await blogModel
       .find(queryData)
-      .find({ isDeleted: false, isPublished: false });
-    if (blogData.length == 0) {
-      return res.status(404).send({
-        status: true,
-        msg: "please provide the correct filters for deletion",
-      });
-    }
+      .findOne({ isDeleted: false });
+
+
+
+    if (!blogData) { return res.status(404).send({ status: false, msg: "blog not found" }) };
+
+
+
     await blogModel.updateMany(queryData, {
       $set: { isDeleted: true, deletedAt: new Date().toLocaleString() },
     });
